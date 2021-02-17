6bc260e149b5856073aefe8888d823ae8ebe683b
wjr-z <1966336874@qq.com>
2021-02-17 12:06:46 +0800

    qwq

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
 <?php
-function str_re($str){
-    $str = str_replace(' ', "", $str);
-    $str = str_replace("\n", "", $str);
-    $str = str_replace("\t", "", $str);
-    $str = str_replace("\r", "", $str);
-    return $str;
+//存有美图链接的文件名img.txt
+$filename = "img.txt";
+if(!file_exists($filename)){
+    die('文件不存在');
 }
-$img=explode("\n",file_get_contents('img.txt'));
-$i=rand(0,count($ing));
-$rand_img=str_re($img[$i]);
-$url='https://tvax3.sinaimg.cn/large/'.$rand_img.'.jpg';
-$result=array("code"=>"200","imgurl"=>"$url");
-$type=$_GET['return'];
-switch ($type)
-{
-	//Json格式解析
-	case 'json':
-	$imageInfo = getimagesize($url);
-	$result['width']="$imageInfo[0]";
-	$result['height']="$imageInfo[1]";
-	header('Content-type:text/json');
-	echo json_encode($result);
-	break;
-	//IMG
-	default:
-	header("Location:".$result['imgurl']);
-	break;
+ 
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+    $line=trim(fgets($fs));
+    if($line!=''){
+        array_push($pics, $line);
+    }
+}
+ 
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+ 
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+ 
+//JSON返回
+case 'json':
+    header('Content-type:text/json');
+    die(json_encode(['pic'=>$pic]));
+ 
+default:
+    die(header("Location: $pic"));
 }
 ?>
