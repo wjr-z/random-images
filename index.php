11542adadbc996f4c53a28808c65d5d9aae0b311
wjr-z <1966336874@qq.com>
2021-02-17 11:28:10 +0800

    qwq

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 $img=file('img.txt');
-$url=array_rand($img);
-header("Location:$img[$url]");
+$url=array_rand($img,1);
+header('Content-Type: image/png');
+echo(file_get_contents($img[$url]));
 exit;
 ?>
