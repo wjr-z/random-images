3e9a57f03ca845cf3ec232e0dec078f2e320c8f0
wjr-z <1966336874@qq.com>
2021-02-17 11:48:17 +0800

    qwq

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-$img=file('img.txt');
-$url=array_rand($img);
-header("Location:https://tvax4.sinaimg.cn/large/0072Vf1pgy1foxkfcikvsj31hc0u0ar1.jpg");
+$img=explode("\n",file_get_contents('img.txt'));
+$i=rand(0,count($ing));
+$rand_img=str_re($img[$i]);
+$url='https://tvax3.sinaimg.cn/large/'.$rand_img.'.jpg';
+$result=array("code"=>"200","imgurl"=>"$url");
+header("Location:".$result['imgurl']);
+function str_re($str){
+  $str = str_replace(' ', "", $str);
+  $str = str_replace("\n", "", $str);
+  $str = str_replace("\t", "", $str);
+  $str = str_replace("\r", "", $str);
+  return $str;
+}
 ?>
