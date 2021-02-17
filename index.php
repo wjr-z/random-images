e175879fdac0d174cb0b9ad1572eada50b2a3852
wjr-z <1966336874@qq.com>
2021-02-17 11:34:24 +0800

    qwq

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 $img=file('img.txt');
-$url=array_rand($img,1);
-header('Content-Type: image/png');
-echo(file_get_contents($img[$url]));
-exit;
+$url=array_rand($img);
+header("Location:https://tvax4.sinaimg.cn/large/0072Vf1pgy1foxkfcikvsj31hc0u0ar1.jpg");
 ?>
